3ba6fcdd413fd11af7e0944ff33c5583bca32dfa
paulbishop69 <paul.bishop@digital.landregistry.gov.uk>
2015-10-01 11:36:27 +0100

    testing out adding feature and rb files direct to master

--- tests/Example3/features/incorrect_login.feature
+++ tests/Example3/features/incorrect_login.feature
@@ -4,6 +4,6 @@ Feature: User logs in and fills in wrong details
 
 
 Scenario: Log in and complete details
-Given i have logged in
-When i enter wrong details
+Given i have navigated to the login page
+When i submit wrong details
 Then an error message is displayed
---
+++ tests/Example3/features/steps/incorrect_login.rb
@@ -0,0 +1,11 @@
+Given(/^i have navigated to the login page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^i submit wrong details$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^an error message is displayed$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
